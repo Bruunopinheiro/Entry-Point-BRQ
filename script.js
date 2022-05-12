339fc69042d8b29385ac40351d22c48999d85bfa
Bruno <brunopinheiro3@yahoo.com.br>
2022-05-11 23:47:40 -0300

    Aprendendo JS

---
+++ script.js
@@ -0,0 +1,13 @@
+var nota1 = 7.8
+var nota2 = 3.4;
+var nota3 = 5;
+
+var mediaFinal = (nota1 + nota2 + nota3) / 3;
+
+console.log(mediaFinal);
+
+var AnoAtual = 2022;
+var anoDeNascimento = 1982;
+
+var idade = AnoAtual - anoDeNascimento;
+console.log(idade);
